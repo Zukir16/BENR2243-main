4790ce38c898e5d47dd3a6fbbd89b4cb9af93138
Zukir16 <b022210146@student.utem.edu.my>
2024-03-28 15:20:13 +0800

    Mongo Basic Functions

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.listen(port, () => {
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://Syakir:Syakir2002_@clustersyakir.pwlh1bs.mongodb.net/?retryWrites=true&w=majority&appName=ClusterSyakir";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,9 +30,43 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
+
+    //await client.db("admin").command({ ping: 1 });
+    //console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    //let result = await client.db('ClusterSyakir').collection('students').insertOne({ 
+      //name: "Syakir",
+      //age: 19,
+      //status: 'A',
+  //})
+  
+    //let result = await client.db('ClusterSyakir').collection('students').updateOne(
+      //{ _id: new ObjectId('66051007d636bde827e269ed') },
+      //{ $set: { name: 'Afif' } }
+    //)
+  //console.log(result)
+
+  //console.log("Ridhuwan added to the database with the id: " + result.insertedId)
+
+  //let result = await client.db('ClusterSyakir').collection('students').find().toArray();
+  //console.log(result)
+
+  //let result = await client.db('ClusterSyakir').collection('students').find(
+    //{
+      //name: 'Afif',
+      //age: 19
+    //}
+  //).toArray()
+  //console.log(result)
+
+  let result = await client.db('ClusterSyakir').collection('students').deleteOne(
+    {
+      name: 'Syakir'
+    }
+  ).toArray()
+  console.log(result)
+
+} finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
